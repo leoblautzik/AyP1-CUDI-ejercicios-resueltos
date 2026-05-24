e8f973edb2d27d7cdcc6e2b4256d5aaa3429214d
leoblautzik <leoblautzik@gmail.com>
2026-05-24 20:02:52 -0300

    Se completa la class Cubo

--- workspace-ayp1-2026/GENC/src/geometria/Cubo.java
+++ workspace-ayp1-2026/GENC/src/geometria/Cubo.java
@@ -1,5 +1,63 @@
 package geometria;
 
 public class Cubo {
+	
+	private double lado;
+
+    /**
+     * pre : longitudLado es un valor mayor a 0.
+     * post: inicializa el cubo a partir de la longitud de lado dada
+     */
+    public Cubo(double longitudLado) {
+        this.setLado(longitudLado);
+    }
+
+    /**
+     * post: devuelve la longitud de todos los lados del cubo
+     */
+    public double getLado() {
+        return this.lado;
+    }
+
+    /**
+     * pre : longitudLado es un valor mayor a 0.
+     * post: cambia la longitud de todos los lados del cubo
+     */
+    public void setLado(double longitudLado) {
+        if (longitudLado <= 0) {
+            throw new Error("El lado debe ser mayor que cero");
+        }
+        this.lado = longitudLado;
+    }
+
+    /**
+     * post: devuelve el área de la superficie de las caras del cubo
+     */
+    public double obtenerAreaCara() {
+    	return Math.pow(lado, 2);
+    }
+
+    /**
+     * pre: areaCara es un valor mayor a 0.
+     * post: cambia el área de las caras del cubo
+     */
+    public void cambiarAreaCara(double areaCara) {
+    	this.setLado(Math.sqrt(areaCara));
+    }
+
+    /**
+     * post: devuelve el volumen que encierra el cubo
+     */
+    public double obtenerVolumen() {
+    	return Math.pow(lado, 3);
+    }
+
+    /**
+     * pre: volumen es un valor mayor a 0.
+     * post: cambia el volumen del cubo
+     */
+    public void cambiarVolumen(double volumen) {
+    	this.setLado(Math.cbrt(volumen));
+    }
 
 }
