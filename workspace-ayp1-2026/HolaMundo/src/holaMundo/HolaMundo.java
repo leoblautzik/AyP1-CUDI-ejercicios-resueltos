49bdcde6e4b96f678a964afeefd0284b8bf0047e
leoblautzik <leoblautzik@gmail.com>
2026-06-04 21:49:05 -0300

    GARR2 - a1

--- workspace-ayp1-2026/HolaMundo/src/holaMundo/HolaMundo.java
+++ workspace-ayp1-2026/HolaMundo/src/holaMundo/HolaMundo.java
@@ -3,24 +3,8 @@ package holaMundo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		
-		int[][] cuadradoMagico = {{6,1,8},{7,5,3},{2,9,4}};
-		
-		for (int i = 0; i<cuadradoMagico.length; i++) {
-			for(int j=0; j< cuadradoMagico[i].length; j++) {
-				System.out.print(cuadradoMagico[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
-		System.out.println("------------");
-				
-		for (int[] fila : cuadradoMagico) {
-			for (int dato : fila) {
-				System.out.print(dato + " ");
-			}
-			System.out.println();
-		}
+		System.out.println("Hola Mundo");
 	}
+		
 
 }
